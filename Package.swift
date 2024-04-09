b649ec4af159d510fd858e74c0f89795760838bb
Alan Charles <alan.charles@segment.com>
2024-04-09 14:32:15 -0600

    chore: update appsflyer-sdk

--- Package.swift
+++ Package.swift
@@ -25,9 +25,9 @@ let package = Package(
             from: "1.5.5"
         ),
         .package(
-            name: "AppsFlyerLib",
-            url: "https://github.com/AppsFlyerSDK/AppsFlyerFramework",
-            from: "6.13.1"
+            name: "AppsFlyerLib-Dynamic",
+            url: "https://github.com/AppsFlyerSDK/AppsFlyerFramework-Dynamic",
+            from: "6.14.0"
         )
     ],
     targets: [
@@ -35,7 +35,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SegmentAppsFlyer",
-            dependencies: ["Segment", "AppsFlyerLib"]),
+            dependencies: ["Segment", "AppsFlyerLib-Dynamic"]),
         
         // TESTS ARE HANDLED VIA THE EXAMPLE APP.
     ]
